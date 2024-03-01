fe2e4939833aac779e34e7e9825c6c32755edb11
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-01 22:31:27 +0545

    chore: ga added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-526TQ3DS61'
+      }
+    })
+  ]
 })
