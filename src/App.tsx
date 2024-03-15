3ab03ce87ce43bbf2b368bb4cb46eb93446fd6ae
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-15 22:45:21 +0545

    feat: alert handling done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import { Output } from './types/output.type'
 import Form from './components/form'
 import Result from './components/result'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const [result, setResult] = useState<Output>([])
 
   return (
     <div className="p-4">
+      <ToastContainer hideProgressBar closeButton={false} stacked />
       <h1 className="text-2xl mb-4">Facebook Story Extractor</h1>
       <Form setResult={setResult} />
       <Result result={result} />
--- src/components/form/utils/handleFormSubmit.ts
+++ src/components/form/utils/handleFormSubmit.ts
@@ -1,6 +1,13 @@
 import { Dispatch, FormEvent, SetStateAction } from 'react'
 import { extractFbViewSource } from '../../../utils/extractFbViewSource'
 import { Output } from '../../../types/output.type'
+import { showAlert } from '../../../utils/alert'
+import { z } from 'zod'
+
+const ContentSchema = z.object({
+  link: z.string().url(),
+  dom: z.string().min(50)
+})
 
 export const handleSubmit = (
   e: FormEvent<HTMLFormElement>,
@@ -8,13 +15,24 @@ export const handleSubmit = (
   clearViewSource: () => void
 ) => {
   e.preventDefault()
+  setResult([])
 
   const formData = new FormData(e.currentTarget)
+  const link = formData.get('link')
   const dom = formData.get('dom')
-  if (!dom) return
 
-  const extractedResults = extractFbViewSource(dom.toString())
-  if (extractedResults == null) return setResult([])
+  const parsedFormData = ContentSchema.safeParse({ link, dom })
+  if (parsedFormData.success === false) return showAlert('Check your inputs')
+
+  const {
+    data: { dom: parsedDom }
+  } = parsedFormData
+
+  const extractedResults = extractFbViewSource(parsedDom)
+  if (extractedResults == null || typeof extractedResults === 'string' || typeof extractedResults === 'number') {
+    showAlert('Story not found!!!')
+    return setResult([])
+  }
 
   setResult(extractedResults)
   e.currentTarget.reset()
